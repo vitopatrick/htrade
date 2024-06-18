18ffb2c240896898c13a843a8df9cafff2ea7aa6
warlock <fecypher@gmail.com>
2024-06-18 16:56:45 +0100

    noe

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -16,28 +16,26 @@ export default function RootLayout({
   const [active, setActive] = useState(false);
 
   return (
-    <html lang="en">
-      <ChartProvider>
-        <body className="bg-bg">
-          {/* parent div container */}
-          <section className="flex h-screen w-screen ">
-            {/* sidebar */}
-            <MobileSideBar isOpen={active} toggle={setActive} />
-            {/* end of sidebar */}
-            <aside className="w-[300px] hidden md:block bg-sideBar h-screen">
-              <SideBarLinks />
-            </aside>
-            <UserProvider>
-              <main className="w-full overflow-auto min-h-0">
-                {/* the header for the pages */}
-                <DashboardHeader toggle={setActive} />
-                {children}
-              </main>
-            </UserProvider>
-          </section>
-          {/* end of parent div container */}
-        </body>
-      </ChartProvider>
-    </html>
+    <ChartProvider>
+      <>
+        {/* parent div container */}
+        <section className="flex h-screen w-screen bg-bg">
+          {/* sidebar */}
+          <MobileSideBar isOpen={active} toggle={setActive} />
+          {/* end of sidebar */}
+          <aside className="w-[300px] hidden md:block bg-sideBar h-screen">
+            <SideBarLinks />
+          </aside>
+          <UserProvider>
+            <main className="w-full overflow-auto min-h-0">
+              {/* the header for the pages */}
+              <DashboardHeader toggle={setActive} />
+              {children}
+            </main>
+          </UserProvider>
+        </section>
+        {/* end of parent div container */}
+      </>
+    </ChartProvider>
   );
 }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig
